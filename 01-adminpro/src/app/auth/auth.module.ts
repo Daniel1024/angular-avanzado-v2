5c1dfce8ae7a33d047d0687b22e036637e7c6d09
Daniel Lopez <d.lopez.1740@gmail.com>
2020-12-05 17:24:21 -0300

    Uso de Scripts de archivos importados en el index.html en TypeScript

--- 01-adminpro/src/app/auth/auth.module.ts
+++ 01-adminpro/src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -15,7 +16,8 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
--- 01-adminpro/src/app/pages/pages.component.ts
+++ 01-adminpro/src/app/pages/pages.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function customInitFunctions(): void;
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -10,6 +12,8 @@ export class PagesComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    customInitFunctions();
+  }
 
 }
