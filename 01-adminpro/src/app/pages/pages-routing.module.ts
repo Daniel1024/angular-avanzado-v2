6e4b95c8c9c96e34d55743c194e5c6e20a685671
Daniel Lopez <d.lopez.1740@gmail.com>
2020-12-05 18:49:49 -0300

    Optimizaciones del Breadcrumbs

--- 01-adminpro/src/app/pages/pages-routing.module.ts
+++ 01-adminpro/src/app/pages/pages-routing.module.ts
@@ -12,10 +12,10 @@ const routes: Routes = [
     path: 'dashboard',
     component: PagesComponent,
     children: [
-      { path: '', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: 'account-settings', component: AccountSettingsComponent }
+      { path: '', component: DashboardComponent, data: { title: 'Dashboard'} },
+      { path: 'progress', component: ProgressComponent, data: { title: 'Progress'} },
+      { path: 'grafica1', component: Grafica1Component, data: { title: 'Charts'} },
+      { path: 'account-settings', component: AccountSettingsComponent, data: { title: 'Account Settings'} }
     ]
   }
 ];
--- 01-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ 01-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
+  subs$: Subscription[] = [];
+  title = '';
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.subs$[0] = this.router.events.pipe(
+      filter(event => event instanceof ActivationEnd),
+      filter(event => (event as ActivationEnd).snapshot.firstChild === null),
+      map(event => (event as ActivationEnd).snapshot.data),
+    ).subscribe(({ title }) => {
+      this.title = title;
+      document.title = `AdminPro - ${title}`;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subs$.forEach(sub => sub.unsubscribe());
   }
 
 }
